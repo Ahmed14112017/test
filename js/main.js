98a42468d157fb26f02d9818a94dbdc313035353
Ahmed14112017 <70607269+Ahmed14112017@users.noreply.github.com>
2021-10-19 07:45:20 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 var say = document.getElementById("sayhi");
-var select = document.getElementById("h");
-var selectoption = select.options[select.selectedIndex].value;
+
+
+
 say.onclick=function(){
-    var x =document.getElementById(x);
-    var y = document.getElementById(y);
+    var select = document.getElementById("o");
+    var selectoption = select.value;
+    console.log(selectoption);
+    var x =document.getElementById("x").value;
+    var y = document.getElementById("y").value;
     if(selectoption =="+"){
-        document.getElementById("show").innerHTML = x + y;
+        document.getElementById("show").innerText =[parseInt(x)] + [parseInt(y)];
     }
     else if(selectoption =="-"){
-        document.getElementById("show").innerHTML = x-y;
+        document.getElementById("show").innerText = x - y;
     }
     else if(selectoption =="*"){
-        document.getElementById("show").innerHTML = x*y;
+        document.getElementById("show").innerText = x*y;
+        
     }
     else{
-        document.getElementById("show").innerHTML = x/y;
+        document.getElementById("show").innerText = x/y;
     }
-    console.log(select.value);
-    console.log(x+y);
+    // console.log(select.value);
+    // console.log(x+y);
+    // console.log(x*y);
+}
+// function changeFunc() {
+// var selectBox = document.getElementById("selectBox");
+// var selectedValue = selectBox.options[selectBox.selectedIndex].innerHTML;
+// alert(selectedValue);
+// }
+
+var users =[];
+
+document.getElementById("add").onclick=function(){
+    item = document.getElementById("input").value;
+    users.push(item);
+    console.log(users);
+    console.log(users.length);
+    document.getElementById("input").value="";
 }
